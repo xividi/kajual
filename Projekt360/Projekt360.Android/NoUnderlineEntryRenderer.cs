67a5c46dadcfb816ba37f71a27513c4f20e3f442
Kaj Skubiszak <kaj.s@devpark.pl>
2023-02-24 11:37:39 +0100

    first commit

---
+++ Projekt360/Projekt360.Android/NoUnderlineEntryRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace Projekt360.Droid
+{
+    public class NoUnderlineEntryRenderer : EntryRenderer
+    {
+        public NoUnderlineEntryRenderer(Context context) : base(context) { }    
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            this.Control.Background = null;
+        }
+    }
+}
